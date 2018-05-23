caab63e304c0ac63cd1893677cf7cea778b11e4d
Christian Zufferey <christian@zufferey.com>
2018-05-23 18:45:19 +0200

    - amélioré le script start.sh afin qu'il arrête et détruise un container couchd si existant

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 #Petit script pour lancer la db couchdb via un docker
-#zf180523.1824
+#zf180523.1843
 # source: https://hub.docker.com/r/library/couchdb/ et http://couchdb.apache.org/
+
+docker container stop my-couchdb
+docker container rm my-couchdb
 
 docker run -d -p 5984:5984 -v $(pwd):/opt/couchdb/data --name my-couchdb couchdb
 
@@ -9,6 +12,7 @@ echo ""
 echo "après 2 minutes, vous pouvez le tester avec:"
 echo ""
 echo "curl http://127.0.0.1:5984/"
+echo ""
 
 
 
